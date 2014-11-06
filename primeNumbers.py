c8b82fe56ddaec1fd7277539d0d9b760614ee15b
Greg <richar45@students.rowan.edu>
2014-11-06 09:48:59 -0500

    Update primeNumbers.py
    I added the class like I think he wanted us to do and other than that I just fixed a few syntax things so it would compile in Pycharm. I added parentheses around the print statement because the compiler was yelling at me if I i didn't but it may work just fine with whatever you were using.  I'm not too sure because I'm just learning python.

--- primeNumbers.py
+++ primeNumbers.py
@@ -3,33 +3,40 @@ import math
 
 
 #########
-#Tests whether a given number is prime or not.
+#Class Prime_Numbers tests whether a given number is prime or not.
 #Returns true if it's a prime, false otherwise
 #########
-def is_prime(num):
-	for j in range(2, num):
-		#num % j would be 0 if j is a divisor of num, making num not a prime
-		if((num % j) == 0):
-			return False
-	return True
+class Prime_Number:
 
-#########
-#Lists all prime numbers less than or equal to the given value.
-#########
-def list_primes(num):
-	print 'All prime numbers less than' 
-	print num 
-	print 'are'
-	for i in range(2, num+1):
-		if(is_prime(i)):
-			print i,
+
+	def __init__(self):
+		print("")
+
+	def is_prime(self, num):
+		for j in range(2, num):
+			#num % j would be 0 if j is a divisor of num, making num not a prime
+			if((num % j) == 0):
+				return False
+		return True
+
+	#########
+	#Lists all prime numbers less than or equal to the given value.
+	#########
+	def list_primes(self, num):
+		print('All prime numbers less than')
+		print(num)
+		print('are')
+		for i in range(2, num+1):
+			if(self.is_prime(i)):
+				print(i),
 
 #########
 #Main function that calls the other functions.
 #########
 def main():
-	upperLimit = int(raw_input('Please enter a number you wish to find all prime numbers less than.'))
-	list_primes(upperLimit)
+	prime = Prime_Number()
+	upperLimit = int(input('Please enter a number you wish to find all prime numbers less than.'))
+	prime.list_primes(upperLimit)
 
 
 main()
